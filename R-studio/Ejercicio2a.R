1e3e88a2d8ae58c77a07b76272387e01d5e6afbe
giffunis <giffuniscode@outlook.com>
2025-08-26 18:30:54 +0100

    La predicción devulve 1

--- R-studio/Ejercicio2a.R
+++ R-studio/Ejercicio2a.R
@@ -1,29 +1,23 @@
 # load the library first
 library(caret)
 
-set.seed(10)
 
 # load the data
-data <- read.csv("~/BigDataProject/R-studio/ficheros_entrada/prediccion.txt"
-                     , header = FALSE, dec = ".", sep = ",", nrows = 30)
+data <- read.csv("~/BigDataProject/R-studio/ficheros_entrada/prediccion.txt", header = FALSE, dec = ".", sep = ",", nrows = 30)
 
-measures <- data[, 2:145]
-nextAverage <- data[, 146]
+# Rename two columns
+colnames(data)[1] <- "date"
+colnames(data)[146] <- "nextAverage"
 
-dataFull <- cbind(measures, nextAverage)
-
-colnames(dataFull)[145] <- "nextAverage"
-
-
-inTrain <- createDataPartition(dataFull$nextAverage, p = 0.7, list = FALSE)
+set.seed(10)
+inTrain <- createDataPartition(data$nextAverage, p = 0.7, list = FALSE)
+training <- data[ inTrain,]
+testing <- data[-inTrain,]
 
 
-training <- dataFull[ inTrain,]
-testing <- dataFull[-inTrain,]
-
-nnetFit <- train(nextAverage ~ ., data = training, method = "nnet")
+nnetFit <- train(nextAverage ~ .
+                  , data = training, method = "nnet")
 
 pred <-predict(nnetFit,testing)
-View(pred)
 
-confusionMatrix(data = pred, testing$nextAverage)
+postResample(pred, testing$nextAverage)
